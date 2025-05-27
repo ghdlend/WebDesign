aa57c884bbbe876257c7b0199fe8fd189cc28a91
Park Jinseong <144192511+ghdlend@users.noreply.github.com>
2025-05-27 17:12:20 +0900

    와이어프레임과 header부분

---
+++ Chapter1/js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+    //메뉴
+    $("nav>ul>li").mouseenter(function(){
+        $(this).children(".sub").stop().slideDown();
+    })
+    $("nav>ul>li").mouseleave(function(){
+        $(".sub").stop().slideUp();
+    })
+})
